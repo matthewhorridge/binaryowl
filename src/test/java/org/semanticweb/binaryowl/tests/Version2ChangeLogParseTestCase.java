373bb39ed24c3cbd271191d2fa3b46e3c3adc79a
Matthew Horridge <matthew.horridge@stanford.edu>
2015-06-04 15:29:07 -0700

    Implemented V2 OntologyChangeRecordList
    - Added an IRI look up table
    - Augmented tests to ensure that V1 and V2 change record lists can be
    parsed correctly.
    - Added the ability to push and pop LookupTables to a stack in a
    BinaryOWLInputStream so that nested lookup tables can be used.

---
+++ src/test/java/org/semanticweb/binaryowl/tests/Version2ChangeLogParseTestCase.java
@@ -0,0 +1,59 @@
+package org.semanticweb.binaryowl.tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.semanticweb.binaryowl.BinaryOWLChangeLogHandler;
+import org.semanticweb.binaryowl.BinaryOWLOntologyChangeLog;
+import org.semanticweb.binaryowl.change.OntologyChangeRecordList;
+import org.semanticweb.binaryowl.chunk.SkipSetting;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+
+import java.io.BufferedInputStream;
+import java.net.URL;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 04/06/15
+ */
+public class Version2ChangeLogParseTestCase {
+
+    private URL changeDataURL;
+
+    private OWLDataFactory df;
+
+    private int counter = 0;
+
+    private static final int EXPECTED_CHANGE_COUNT = 323;
+
+    @Before
+    public void setUp() throws Exception {
+        changeDataURL = Version1ChangeLogParseTestCase.class.getResource("/change-data-v2.binary");
+        df = OWLManager.getOWLDataFactory();
+    }
+
+    @Test
+    public void shouldParseVersion2ChangeLog() throws Exception {
+        try {
+            final BufferedInputStream inputStream = new BufferedInputStream(changeDataURL.openStream());
+            final BinaryOWLOntologyChangeLog log = new BinaryOWLOntologyChangeLog();
+            log.readChanges(inputStream, df, new BinaryOWLChangeLogHandler() {
+                @Override
+                public void handleChangesRead(OntologyChangeRecordList list, SkipSetting skipSetting, long filePosition) {
+                    counter += list.getChangeRecords().size();
+                }
+            });
+            inputStream.close();
+            assertThat(counter, is(EXPECTED_CHANGE_COUNT));
+        } catch (Exception e) {
+            fail("Parse failed with exception: " + e);
+            e.printStackTrace();
+        }
+
+    }
+}
